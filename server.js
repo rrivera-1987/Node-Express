6daf81e8ca3802585f90b57009d919d4ab675f86
Ramon A. Rivera <ramonrivera550@gmail.com>
2023-10-22 22:24:53 -0400

    Express Simple REST Part 1

--- server.js
+++ server.js
@@ -8,6 +8,51 @@ const port = 3000;
 const app = express();
 app.use(morgan('dev'));
 
+//Setting up express router
+app.use(express.json());
+
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsites: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} 
+        with description ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
 app.use(express.static(__dirname + '/public'));
 
 /* Setting up the server */
@@ -28,4 +73,10 @@ In express, a middleware function has access to three parameters req, res and ne
 Setting up morgan: initiating morgan without a callback. Instead use the morgan function with the argument of 'dev'.
 This will configure Morgan to log the development version.
 __dirname: special variable in Node. It will refer to the absolute path of the current directory of the file that is in.
+
+app.use(express.json()): when the server receives request with JSON formatted data in the body, this middleware function
+will handle parsing that data into JS properities of the request object so that we can use that data in JS.
+
+app.all() will cath all HTTP verbs.
+next(): this function will pass control of the application routing to the next relevant routing method.
 */
